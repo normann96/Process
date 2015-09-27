b78f0a9afdc4c44a0332dd836cee7bcc963ab66d
normann96 <alexfeodosia@yandex.ru>
2015-09-27 11:35:42 +0300

    Commit 1

---
+++ Process/Process.cpp
@@ -0,0 +1,37 @@
+// Process.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+#include <tchar.h>
+#include <conio.h>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//char lpszAppName[] = "Notepad.exe D:\\23.txt";
+	STARTUPINFO si = {sizeof(STARTUPINFO)};
+	PROCESS_INFORMATION pi;
+
+	/*ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);*/
+
+	// создаем новый консольный процесс
+	if (!CreateProcess(NULL, "Notepad.exe", NULL, NULL, FALSE, NULL, NULL, NULL, &si, &pi))
+	{
+		cout << "The new process is not created.\n";
+		cout << "Press any key to exit.\n";
+		cin.get();
+		return GetLastError();
+	}
+	
+	cout << pi.dwProcessId << endl;
+	remove("D:\\23.txt");
+
+	system("pause");
+	return 0;
+}
+
+
